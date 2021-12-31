1fdfade40fbbb9b0063b3e6fb7a8bce51b31a695
GILBERTSAJA <93893614+Gilbertgeraldo@users.noreply.github.com>
2021-12-31 08:42:58 +0700

    update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -4,24 +4,24 @@ return `DONASI BISA VIA BANK / E-MONEY
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ -
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ -
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ -
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ -
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 0812 3513 6510
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ tidak tersedia
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 0812 3513 6510
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama GILBERT
 `
 	}
 
